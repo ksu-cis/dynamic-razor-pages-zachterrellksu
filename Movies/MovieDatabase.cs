95e348c65649d680c0df320f58aedc44a76f0df7
Nathan H. Bean <nhbean@ksu.edu>
2020-04-17 14:09:58 -0500

    Initial commit

---
+++ Movies/MovieDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Movies
+{
+    /// <summary>
+    /// A class representing a database of movies
+    /// </summary>
+    public static class MovieDatabase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        /// <summary>
+        /// Loads the movie database from the JSON file
+        /// </summary>
+        static MovieDatabase() {
+            
+            using (StreamReader file = System.IO.File.OpenText("movies.json"))
+            {
+                string json = file.ReadToEnd();
+                movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+            }
+        }
+
+        /// <summary>
+        /// Gets all the movies in the database
+        /// </summary>
+        public static IEnumerable<Movie> All { get { return movies; } }
+    }
+}
